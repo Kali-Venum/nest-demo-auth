5a0b7bde9530e756a477ce1451390d2baf71b79b
Kali-Venum <subhamdas.abcd@gmail.com>
2023-07-30 21:07:41 +0530

    a user array is created.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entity/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  public users: User[] = [
+    {
+      email: 'email1@email.com',
+      password: 'admin',
+      age: 10,
+    },
+    {
+      email: 'email2@email.com',
+      password: 'admin',
+      age: 20,
+    },
+    {
+      email: 'email2@email.com',
+      password: 'admin',
+      age: 30,
+    },
+  ];
+}
