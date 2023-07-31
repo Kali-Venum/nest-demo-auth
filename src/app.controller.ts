23a8d29c723322ae55f6d23f94e0b1f32f29afb4
weaverswebsubham <subham.das@weavers-web.com>
2023-07-31 18:05:48 +0530

    mysql db connected, login & register done.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,41 @@
-import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
-// import { LocalAuthGuard } from './auth/local-auth.guard';
+import * as bcrypt from 'bcrypt';
+import passport from 'passport';
+import { clearScreenDown } from 'readline';
 
 @Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('register')
+  async register(@Body() body: any) {
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+
+    return this.appService.register({
+      name: body.name,
+      email: body.email,
+      password: hashedPassword,
+    });
+  }
+
+  @Post('login')
+  async login(@Body() body: any) {
+    const user = await this.appService.findOne(body.email);
+
+    if (!user) {
+      throw new BadRequestException();
+    } else {
+      const comparedPassword = await bcrypt.compare(
+        body.password,
+        user.password,
+      );
+
+      if (!comparedPassword) {
+        throw new BadRequestException();
+      } else {
+        return user;
+      }
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async register(body: any): Promise<User> {
+    return this.userRepository.save(body);
+  }
+
+  async findOne(email: string): Promise<User> {
+    return await this.userRepository.findOne({
+      where: {
+        email: email
+      }
+    });
   }
 }
