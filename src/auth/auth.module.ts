af6eed73ace2c47af6bd5fe51b59f4d5f8971825
weaverswebsubham <subham.das@weavers-web.com>
2023-07-31 12:05:53 +0530

    local stratergy injected into the providers.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [PassportModule],
-  providers: [AuthService]
+  imports: [PassportModule, UsersModule],
+  providers: [AuthService, LocalStrategy]
 })
 export class AuthModule {}
